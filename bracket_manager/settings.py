6452696148179dd303d5953ea1f1e57c0e709c2b
Tyler Nguyen <tyler.h.nguyen@sjsu.edu>
2022-04-11 18:15:07 -0700

    setup frontend

--- bracket_manager/settings.py
+++ bracket_manager/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-r3pn!16$k138q^!)%iz@rghps=r%59kpsxo1&^e!e3n@7uex#m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+//import HomePage from "./HomePage";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Testing React Code</h1>;
+  }
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
